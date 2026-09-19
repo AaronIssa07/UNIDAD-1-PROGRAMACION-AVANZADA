84498ddc6968cc2a74cfb32a7b2549025f28e4b8
Aarón Isaí Osorio Ruiz <aaronisai09675@gmail.com>
2026-09-18 20:55:42 -0600

    Add files via upload

---
+++ Ejercicio 01 Problemario/Ejercicio 01/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Ejercicio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("EJERCICIO 1: CÁLCULO DE POTENCIA ELÉCTRICA DE UN MOTOR CD"); //Planteamiento del problema      
+            Console.Write("Ingrese el voltaje: ");  // Recopilacion de datos: Solicitar al usuario el voltaje y la corriente
+            double v = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Ingrese la corriente: ");
+            double i = Convert.ToDouble(Console.ReadLine());
+
+                                                          
+            double p = v * i;                       // Operación: Calcular la potencia eléctrica utilizando la fórmula P = V * I
+
+            Console.WriteLine($"\nPotencia calculada: {p} W");
+
+                                                                       
+            if (p <= 120) // Comparacion y clasificacion de la potencia al usuario
+            {
+                Console.WriteLine("Estado: CONSUMO NORMAL"); // Clasificación de la potencia calculada
+            }
+            else
+            {
+                Console.WriteLine("Estado: ADVERTENCIA: CONSUMO ELEVADO"); // Clasificación de la potencia calculada
+            }
+        }
+    }
+}
---
+++ Ejercicio 02 Problemario/Ejercicio 02/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Ejercicio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("EJERCICIO 2: MEDICIÓN DE TEMPERATURA DE UN MOTOR"); //Plantemiento del problema
+            Console.WriteLine("Solicita 10 lecturas de temperatura y determina si el promedio es seguro.\n");
+
+            double suma = 0;    // Variable para almacenar la suma de las temperaturas
+
+            for (int i = 1; i <= 10; i++) 
+            {
+                Console.Write($"Ingrese la medición de temperatura {i} (°C): "); // Solicita la temperatura al usuario
+                double temp = Convert.ToDouble(Console.ReadLine());
+                suma += temp;
+            }
+
+            double promedio = suma / 10.0;                                      //Operacion para calcular el promedio de las temperaturas ingresadas
+            Console.WriteLine($"\nTemperatura Promedio: {promedio:F2} °C");    // Muestra el promedio de las temperaturas ingresadas
+
+            if (promedio <= 70)                                                // Condición para determinar si la temperatura promedio es segura
+            {
+                Console.WriteLine("Estado: TEMPERATURA NORMAL");
+            }
+            else
+            {
+                Console.WriteLine("Estado: ALERTA DE TEMPERATURA");
+            }
+        }
+    }
+}
